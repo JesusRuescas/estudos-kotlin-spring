fee54b25acf01fbb3261d0f580466b79143e66e0
Jesus Ruescas Junior <jesus.junior.1999@hotmail.com>
2024-08-07 11:12:03 -0300

    Initial commit

---
+++ src/main/kotlin/com/estudos/kotlin/spring/estudos_kotlin_spring/EstudosKotlinSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.estudos.kotlin.spring.estudos_kotlin_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EstudosKotlinSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<EstudosKotlinSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/estudos/kotlin/spring/estudos_kotlin_spring/EstudosKotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.estudos.kotlin.spring.estudos_kotlin_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EstudosKotlinSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
